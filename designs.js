21d788abe1a6270678f55d49b2059278b9bbd9a4
echo8023 <34594579+echo8023@users.noreply.github.com>
2018-03-04 21:50:07 +0800

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,24 @@
-// Select color input
-// Select size input
+$(function () {
+    $('#gridSubmit').click(function (event) {
+        event.preventDefault();
+        makeGrid();
+    });
 
-// When size is submitted by the user, call makeGrid()
+    function makeGrid() {
+        const pixelCanvas = $('#pixelCanvas');
+        const gridHeight = $('#inputHeight').val();
+        const gridWidth = $('#inputWeight').val();
+        pixelCanvas.empty();
+        // creates the table grid
+        for (let r = 0; r < gridHeight; r++) {
+            let row = $('<tr class="canvasRow"></tr>').appendTo(pixelCanvas);
+            for (let c = 0; c < gridWidth; c++) {
+                $('<td class="canvasCol"></td>').appendTo(row);
+            }
+        }
+    }
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+    $('#pixelCanvas').on('click', 'td', function (event) {
+        $(this).css('background-color', $('#colorPicker').val());
+    })
+});
